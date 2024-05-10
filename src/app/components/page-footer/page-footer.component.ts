b55ae4474a555656d32cd6ec182cda08d52e0204
Abhinav Goel <abhinavgo1000@gmail.com>
2024-05-10 16:16:13 -0400

    page footer added

--- src/app/components/page-footer/page-footer.component.ts
+++ src/app/components/page-footer/page-footer.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-page-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './page-footer.component.html',
   styleUrl: './page-footer.component.scss'
 })
 export class PageFooterComponent {
 
+  constructor(
+    private router: Router
+  ) {}
+
+  navigateToHome() {
+    this.router.navigate(['/home']);
+  }
+
+  navigateToBarChart() {
+    this.router.navigate(['/bar-chart']);
+  }
+
+  navigateToLineChart() {
+    this.router.navigate(['/line-chart']);
+  }
+
+  navigateToPieChart() {
+    this.router.navigate(['/pie-chart']);
+  }
+
+  navigateToDataEntry() {
+    this.router.navigate(['/data-entry']);
+  }
 }
