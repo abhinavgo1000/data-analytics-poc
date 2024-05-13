08b4e5fda5640bafca8f464b661d56c2ab439437
Abhinav Goel <abhinavgo1000@gmail.com>
2024-05-13 11:38:51 -0400

    capitalize pipe configured

--- src/app/shared/pipes/capitalize-first.pipe.ts
+++ src/app/shared/pipes/capitalize-first.pipe.ts
@@ -6,8 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CapitalizeFirstPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string, ...args: unknown[]): string {
+    if (!value) {
+      return value;
+    }
+    return value.charAt(0).toUpperCase() + value.slice(1);
   }
 
 }
